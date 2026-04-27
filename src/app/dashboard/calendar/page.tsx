15b4cf3292fc827c06f514b1ec5cbdff215c5d98
PashaBudzin <pashabudzin@proton.me>
2026-04-27 10:04:44 +0300

    feat: make calendar page not cachable to display live data

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -1,6 +1,8 @@
 import { getCasesWithHearings } from "@/lib/actions/cases";
 import CalendarClient from "./calendar-client";
 
+export const dynamic = "force-dynamic";
+
 export default async function CalendarPage() {
   const hearings = await getCasesWithHearings();
 
